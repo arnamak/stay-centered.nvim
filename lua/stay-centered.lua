f0e7489a0311b51c3f56926274336056962de810
Mak Arnautovic <mak@tablexi.com>
2022-07-29 21:21:56 -0500

    Support filetype exceptions

--- lua/stay-centered.lua
+++ lua/stay-centered.lua
@@ -1,17 +1,28 @@
 local ac = vim.api.nvim_create_autocmd
 local ag = vim.api.nvim_create_augroup
 local getCursor = vim.api.nvim_win_get_cursor
+local skip_files = vim.g["stay-centered#skip_filetypes"]
+
+local function config_has_files_to_skip (t, v)
+  if t ~= nil then
+    for index, value in ipairs(t) do
+      if value == v then return true end
+    end
+  end
+end
 
 function StayCentered(inInsert)
-    local line = getCursor(0)[1]
-    if line ~= vim.b.last_line then
-        vim.cmd('norm! zz')
-        vim.b.last_line = line
-        if inInsert then
-            local column = vim.fn.getcurpos()[5]
-            vim.fn.cursor(line, column)
-        end
+  if config_has_files_to_skip(skip_files, vim.bo.filetype) then return false end
+
+  local line = getCursor(0)[1]
+  if line ~= vim.b.last_line then
+    vim.cmd('norm! zz')
+    vim.b.last_line = line
+    if inInsert then
+      local column = vim.fn.getcurpos()[5]
+      vim.fn.cursor(line, column)
     end
+  end
 end
 
 local group = ag('StayCentered', { clear = true })
