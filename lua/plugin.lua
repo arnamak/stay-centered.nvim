02db99e6730d9b16ffd6d997f0d0335762638e47
nfwyst <nfwyst@gmail.com>
2024-09-07 16:31:17 +0800

    fix(stay_centered): ensure cursor stays in correct column after line deletion

--- lua/plugin.lua
+++ lua/plugin.lua
@@ -24,10 +24,10 @@ local function stay_centered(ctx)
 
 	local line = vim.api.nvim_win_get_cursor(0)[1]
 	if line ~= vim.b.last_line then
+		local column = vim.fn.getcurpos()[3]
 		vim.cmd("norm! zz")
 		vim.b.last_line = line
 		if ctx.mode == mode.insert then
-			local column = vim.fn.getcurpos()[5]
 			vim.fn.cursor({ line, column })
 		end
 	end
